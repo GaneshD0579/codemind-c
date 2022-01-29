4d59584a8a3cb9527491999e96986631b475f1cb
GaneshD0579 <98636302+GaneshD0579@users.noreply.github.com>
2022-01-29 04:58:51 -0300

    #include<stdio.h> int main() {     int n,d;     scanf("%d",&n);     if(n>2)     {         d=n*(n-3)/2;         printf("%d",d);     }     else     {         printf("Its not an polygon");     } }

---
+++ Diagonals_of_polygon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,d;
+    scanf("%d",&n);
+    if(n>2)
+    {
+        d=n*(n-3)/2;
+        printf("%d",d);
+    }
+    else
+    {
+        printf("Its not an polygon");
+    }
+}
